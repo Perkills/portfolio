228c9cfbbcf7927fc18dc4e98bdae6d9ac032ae2
Perkills <32464300+Perkills@users.noreply.github.com>
2018-01-08 00:15:14 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+ function showHide(){
+
+        var e = document.getElementById('design');
+
+    if ( e.style.display != 'none' ) {
+        e.style.display = 'none';
+    }
+    else {
+        e.style.display = '';
+    }
+
+}
